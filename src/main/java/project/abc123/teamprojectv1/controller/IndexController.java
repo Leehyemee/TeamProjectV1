27b323a1d39122aba3ff4313e13d5ee7a4fa88ef
hyemee <svsvdvdv@naver.com>
2025-02-27 14:44:11 +0900

    Initial commit

---
+++ src/main/java/project/abc123/teamprojectv1/controller/IndexController.java
@@ -0,0 +1,11 @@
+package project.abc123.teamprojectv1.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+
+    public  String index() {
+        return "index";
+    }
+}
